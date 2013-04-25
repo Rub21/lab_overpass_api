a2923c4fb3de82c93c8bb514102a98728f2438b1
Rub21 <rub2106@gmail.com>
2013-04-25 09:38:18 -0500

    parametrando zoom

--- app/js/app.js
+++ app/js/app.js
@@ -37,30 +37,31 @@
 
         function download(a, type) {
 
-            var dir = "http://127.0.0.1:8111/";
             var mapzoom = map.getZoom();
-            var locations = (map.getExtent() + '').split(',');
-            //date 
-            var date_hour = $('#datetimepicker input').attr('value');
 
-            var date = date_hour.substring(0, 10).split("/");
-            var hour = date_hour.substring(11, 19).split(":");
-            console.log(date);
-            console.log(hour);
-            //<newer than="' + date[2] + '-' + date[0] + '-' + date[1] + 'T' + hour[0] + ':' + hour[1] + ':' + hour[2] + 'Z"/>'
-            var query = a + '<query type="' + type + '"> <newer than="' + date[2] + '-' + date[0] + '-' + date[1] + 'T' + hour[0] + ':' + hour[1] + ':' + hour[2] + 'Z"/>' + //2013-04-21T00:00:00Z
-            '<bbox-query s="' + locations[2] + '" n="' + locations[0] + '" w="' + locations[1] + '" e="' + locations[3] + '"/>' +
-                '</query> <print mode="meta"/>';
-            console.log(query);
-            $.get(dir + "import", {
-                url: 'http://overpass-api.de/api/interpreter?data=' + query
-            }).error(function() {
-                alert("Error: Enable JOSM remote!")
-            }).success(function() {
-                e.dialog("close")
-            });
+            if (mapzoom >= 7) {
+                var dir = "http://127.0.0.1:8111/";
+                var locations = (map.getExtent() + '').split(',');                //date 
+                var date_hour = $('#datetimepicker input').attr('value');
+                var date = date_hour.substring(0, 10).split("/");
+                var hour = date_hour.substring(11, 19).split(":");
+                console.log(date);
+                console.log(hour);
+                //<newer than="' + date[2] + '-' + date[0] + '-' + date[1] + 'T' + hour[0] + ':' + hour[1] + ':' + hour[2] + 'Z"/>'
+                var query = a + '<query type="' + type + '"> <newer than="' + date[2] + '-' + date[0] + '-' + date[1] + 'T' + hour[0] + ':' + hour[1] + ':' + hour[2] + 'Z"/>' + //2013-04-21T00:00:00Z
+                '<bbox-query s="' + locations[2] + '" n="' + locations[0] + '" w="' + locations[1] + '" e="' + locations[3] + '"/>' +
+                    '</query> <print mode="meta"/>';
+                console.log(query);
+                $.get(dir + "import", {
+                    url: 'http://overpass-api.de/api/interpreter?data=' + query
+                }).error(function() {
+                    alert("Error: Enable JOSM remote!")
+                }).success(function() {
+                    e.dialog("close")
+                });
+            }else{
 
+                alert("zoom in a little so we don't have to load a huge area from the API.")
+            }
         };
-
-
     })();
